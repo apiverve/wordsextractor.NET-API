a2cad6ba3b36ceeb474b2c25e0b08f97469e37a1
APIVerve Team <hello@apiverve.com>
2024-05-14 15:46:30 -0500

    Bug fixes and Improvements

---
+++ wordsextractorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class wordsextractorQueryOptions {
+public string text { get; set; }
+public string partofspeech { get; set; }
+}
+
+}
